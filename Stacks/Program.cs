a5afbd102fd746e72856aee8f5dfbf05810eeebb
Abdullahi Idris <44320047+abdul-lahidris@users.noreply.github.com>
2021-05-22 19:23:16 +0100

    Stacks

---
+++ Stacks/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Stacks!");
+            Stack myStack = new Stack();
+            myStack.top = null;
+
+            StackOperations operations = new StackOperations();
+
+            operations.push(myStack, 1);
+            operations.push(myStack, 2);
+            operations.push(myStack, 3);
+            operations.push(myStack, 4);
+            operations.push(myStack, 5);
+
+            operations.printStack(myStack);
+
+            Console.WriteLine("\n\nPop stack");
+            operations.pop(myStack);
+            operations.printStack(myStack);
+            Console.WriteLine("\n\nPop stack again");
+            operations.pop(myStack);
+            operations.printStack(myStack);
+            Console.WriteLine("\n\nPush 23 to stack");
+            operations.push(myStack, 23);
+            operations.printStack(myStack);
+
+        }
+    }
+}
---
+++ Stacks/StackOperations.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stacks
+{
+    internal class Node
+    {
+        internal int data;
+        internal Node next;
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+        public Node(char d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+    internal class Stack
+    {
+        internal Node top;
+    }
+    class StackOperations
+    {
+        internal void push(Stack myStack, int data)
+        {
+            Node newNode = new Node(data);
+            newNode.next = myStack.top;
+            myStack.top = newNode;
+        }
+
+        internal void pop(Stack myStack)
+        {
+            Node temp = myStack.top;
+            if (temp == null)
+                return;  
+            myStack.top = temp.next;
+            temp.next = null;
+        }
+
+        internal void printStack(Stack myStack)
+        {
+            Node temp = myStack.top;
+            while (temp != null)
+            {
+                Console.Write($"{temp.data} ");
+                if (temp.next == null)
+                    return;
+                temp = temp.next;
+            }
+        }
+
+    }
+
+}
